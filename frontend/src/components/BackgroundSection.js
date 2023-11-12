4ca7cbf9c11771223ac946650a5428936d77682a
enoskyeza <enoskyeza@gmail.com>
2023-11-12 16:54:11 +0300

    added components dir

---
+++ frontend/src/components/BackgroundSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BackgroundSection.css';
+
+const BackgroundSection = () => {
+    return (
+        <section className='section'>
+            <div className='right-rectangle'></div>
+            <div className='left-rectangle'></div>
+            {/* Other content goes here */}
+        </section>
+    );
+}
+
+export default BackgroundSection
