5e5b0004aedaedb46e51950597d32c9045e68471
enoskyeza <enoskyeza@gmail.com>
2023-11-13 01:04:03 +0300

    fixed layout

--- frontend/src/components/BackgroundSection.js
+++ frontend/src/components/BackgroundSection.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Navigationbar from "./Navigationbar";
 import MainSection from "./MainSection";
 import './BackgroundSection.css';
@@ -9,21 +7,16 @@ import './BackgroundSection.css';
 
 const BackgroundSection = () => {
     return (
-        <section className='section'>
+        <Container className='section-' fluid>
             <div className='bottom-section'>
+                <div className='nav-section-menu'><Navigationbar /></div>
                 <div className='right-rectangle'></div>
                 <div className='left-rectangle'></div>
             </div>
-                <Row className='top-section'>
-                    <Col xs={8}>
-                        <MainSection />
-                    </Col>
-                    <Col>
-                        <Navigationbar />
-                    </Col>
-                </Row>
-        </section>
-
+            <div className='top-section'>
+                    <MainSection />
+            </div>
+        </Container>
     );
 }
 
--- frontend/src/components/MainSection.js
+++ frontend/src/components/MainSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
 import './MainSection.css';
 import profile from '../images/profile.jpeg'
@@ -9,10 +10,10 @@ import profile from '../images/profile.jpeg'
 const MainSection = () => {
     return (
             <Row className='main-section'>
-                <Col className='image-container'>
-                    <Image src={profile} fluid />
+                <Col md={4} className='image-container'>
+                    {/*<Image src={profile} />*/}
                 </Col>
-                <Col className='biography-info'>
+                <Col md={7} className='biography-info'>
                         <h2><span className='name-el'>I'M STEVE MILNER.</span> WEB DESIGNER</h2>
                         <p>I'm a Tunisian based web designer & front‑end developer focused on crafting clean
                             & user‑friendly experiences, I am passionate about building excellent software that
